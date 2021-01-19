26e918bbcda7b63299ddb4e57f0675bc0d4b9e7e
Niccolo Delfino <delfino.nicco@libero.it>
2021-01-19 16:52:37 +0100

    Init

---
+++ Esercizio2/js/script.js
@@ -0,0 +1,3 @@
+// Utilizzando const e/o let e template literal. Creare due tag div con due id diversi.
+// Un div avrà il testo colorato di rosso mentre l’altro di verde.
+// Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
