69ba54b1f8b1ef6b5f494469c9829fe85c474bb9
Niccolo Delfino <delfino.nicco@libero.it>
2021-01-19 17:33:58 +0100

    Esercizio3 Init

---
+++ Esercizio3/js/script.js
@@ -0,0 +1,3 @@
+// Utilizzando const e/o let. Dare la possibilità di inserire due parole.
+// Verificare se le due parole hanno la stessa lunghezza.
+// Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
