ce604403736d96702fdc2f167e54f90b7a419960
Niccolo Delfino <delfino.nicco@libero.it>
2021-01-20 16:00:52 +0100

    Array ok

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,2 +1,17 @@
 // Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore utilizzando destructuring e template literal.
+
+const bici = [
+  {
+    nome : 'Easy',
+    peso : 20
+  },
+  {
+    nome : 'Pro',
+    peso : 40
+  },
+  {
+    nome : 'Standard',
+    peso : 30
+  }
+];
