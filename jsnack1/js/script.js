2897296626ecaf90b7b1e6ff5e60a5b739627965
Niccolo Delfino <delfino.nicco@libero.it>
2021-01-20 15:49:25 +0100

    Init

---
+++ jsnack1/js/script.js
@@ -0,0 +1,2 @@
+// Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal.
